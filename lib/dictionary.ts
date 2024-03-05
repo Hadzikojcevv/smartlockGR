14b805604fee79a5f320ec981889495e985f4100
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2024-03-06 00:19:54 +0100

    locales

--- lib/dictionary.ts
+++ lib/dictionary.ts
@@ -2,8 +2,8 @@ import 'server-only'
 import type { Locale } from '@/i18n.config'
 
 const dictionaries = {
-  en: () => import('@/dictionaries/en.json').then(module => module.default),
-  gr: () => import('@/dictionaries/gr.json').then(module => module.default)
+  gr: () => import('@/dictionaries/gr.json').then(module => module.default),
+  en: () => import('@/dictionaries/en.json').then(module => module.default)
 
 }
 
